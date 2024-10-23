e29fa599542f823b7cace7724f3381f9abbcd812
Johannes Nogueira <johannes.nogueira@eduq.app>
2024-10-23 19:27:04 -0300

    teste

--- src/sources/google_news.py
+++ src/sources/google_news.py
@@ -2,7 +2,7 @@ import requests
 # from bs4 import BeautifulSoup
 import os
 import pandas as pd
-from sources.news import News
+# from sources.news import News
 import time
 import streamlit as st
 
